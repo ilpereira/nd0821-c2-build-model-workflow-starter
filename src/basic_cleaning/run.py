727226ec1d9919528a0d3db562640db256563cac
igor <igor.pereira@olist.com>
2022-03-03 23:02:13 -0300

    fixed handling of dataset rows which are not in the proper geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -32,6 +32,9 @@ def go(args):
     logger.info("Converting 'last_review' column to datetime")
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     df.to_csv(args.output_artifact, index=False)
 
     artifact = wandb.Artifact(
